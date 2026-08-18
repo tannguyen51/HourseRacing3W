24d66eb2497a98875bc078f690e33875e3588705
QuangNhat121 <nhat9983@gmail.com>
2026-08-18 12:54:17 +0700

    fix UI/UX

--- FE/src/pages/OwnerHorseEditPage/OwnerHorseEditPage.jsx
+++ FE/src/pages/OwnerHorseEditPage/OwnerHorseEditPage.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { getHorse, updateHorse } from "../../services/ownerHorseApi";
 import { request, resolveApiUrl } from "../../services/apiClient";
 import { validateHorseStats } from "../../utils/horseValidation";
+import "../OwnerHorseFormPage.css";
 const getFullUrl = (url) => resolveApiUrl(url);
 
 function OwnerHorseEditPage() {
